c569e6d1a35b3bbc0b5ae59b9553633edd2c7175
jpedrosiq <johnpedrosiqueira@gmail.com>
2020-05-12 18:00:32 -0700

    Push to heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const passport = require('passport')
+const path = require('path');
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
@@ -27,5 +28,10 @@ app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 // testing the branch
--- config/keys.js
+++ config/keys.js
@@ -3,3 +3,10 @@ module.exports = {
     "mongodb+srv://dev:XPPhQurcRgDKX20h@cluster0-pb1wa.mongodb.net/WhatFlix?retryWrites=true&w=majority",
   secretOrKey: "WhatFlixKey"
 };
+
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
