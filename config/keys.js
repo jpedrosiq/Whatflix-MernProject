9f6ed7cca3755651c7fbd66b80a404805bad1baa
jpedrosiq <johnpedrosiqueira@gmail.com>
2020-05-12 20:47:12 -0700

    Fix heroku port

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,3 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:XPPhQurcRgDKX20h@cluster0-pb1wa.mongodb.net/WhatFlix?retryWrites=true&w=majority",
-  secretOrKey: "WhatFlixKey"
-};
-
-
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./keys_prod');
 } else {
