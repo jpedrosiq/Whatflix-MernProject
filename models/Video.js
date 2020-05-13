20a8d116a90a5c6a12475d583c7f9390d72404d1
Willie <wongxingyao@gmail.com>
2020-05-12 22:21:13 -0700

    Add video model

---
+++ models/Video.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+//based off of reference code, can be adjusted
+const videoSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+    },
+    privacy: {
+      type: Number,
+    },
+    filePath: {
+      type: String,
+    },
+    //below is changed. original used to be "category: String"
+    catogory: {
+      type: String, 
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = mongoose.model("Video", videoSchema);
+
+module.exports = { Video };
