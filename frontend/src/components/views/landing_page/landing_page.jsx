1ba3e1bddaa91f13a6d4a84d6d387bf3bb4e28ea
jpedrosiq <johnpedrosiqueira@gmail.com>
2020-05-14 14:35:38 -0700

    Implement autoscroll

--- frontend/src/components/views/landing_page/landing_page.jsx
+++ frontend/src/components/views/landing_page/landing_page.jsx
@@ -3,7 +3,7 @@ import {Typhography, Row, Button, Divider} from 'antd';
 import { API_URL, API_KEY, IMAGE_BASE_URL, IMAGE_SIZE, POSTER_SIZE } from '../../Config';
 import MainImage from './sections/main_image';
 import GridCard from './sections/gridCard';
-import BrowseNavBar from "../../browse/browse_navbar"
+import BrowseNavBar from "../../nav/browse_navbar"
 
 // const { Title } = Typhography;
 
